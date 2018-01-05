2c534eccd02ebb8cec1fe464edd681fba0adf5b2
Steve Pagan <spagan@instartlogic.com>
2018-01-05 15:45:47 -0800

    Add Juniper Script to personal Git

--- pushJuniperChanges.py
+++ pushJuniperChanges.py
@@ -4,42 +4,49 @@ from jnpr.junos import Device
 from jnpr.junos.utils.config import Config
 
 
-def pushconfig(user, password, dev):
+def getdevice(user, password, dev):
 
     device = dev.rstrip()
     device_split = device.split(',')
     site = device_split[0].split('-')
     subnet = device_split[1]
-    firewall = Device(host=device_split[0], user=user, password=password)
+    dev = Device(host=device_split[0], user=user, password=password)
+    return dev
 
-    print "Executing commands on", firewall
 
-    connected = False
+def connectionhandler(dev):
 
-    for i in range(0, 100):
-        while True:
-            try:
-                firewall.open()
-                firewall.timeout = 300
-            except Exception as err:
-                print "Cannot connect to device:", err
-                continue
-            connected = True
-            break
-        if connected:
-            break
+    try:
+        dev.open()
+        dev.timeout = 300
+        return dev
+    except Exception:
+        dev = "Could not connect"
+        return dev
 
-    config_set = """  """
-    print config_set
 
-    with Config(firewall, mode='private') as cu:
-        cu.load(config_set, format='set')
-        diff = cu.diff()
-        print diff
-        print "Committing Configuration"
-        cu.commit(timeout=360)
+def pushconfig(user, password, dev):
 
-    firewall.close()
+    device = getdevice(user, password, dev)
+    netdevice = connectionhandler(device)
+
+    if netdevice != "Could not connect":
+
+        print "Executing commands on", netdevice
+
+        config_set = "  "
+        print config_set
+
+        with Config(netdevice, mode='private') as cu:
+            cu.load(config_set, format='set')
+            diff = cu.diff()
+            print diff
+            print "Committing Configuration"
+            cu.commit(timeout=360)
+
+        netdevice.close()
+    else:
+        print "Could not connect to %s, moving to next device" % device
 
 
 if __name__ == "__main__":
