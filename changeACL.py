be089d2ffcf7555dc4a67cfaa1841c65b55cbd79
Steve Pagan <spagan@instartlogic.com>
2018-01-08 16:41:38 -0800

    Add Juniper Script to personal Git

--- changeACL.py
+++ changeACL.py
@@ -9,8 +9,9 @@ from netmiko import ConnectHandler
 def get_args():
     parser = argparse.ArgumentParser()
     parser.add_argument("config", help="Configuration file to log into devices")
-    parser.add_argument("router", help="Router device that will be checked")
+    parser.add_argument("routers", help="File containing routers", type=argparse.FileType('r'))
     args = parser.parse_args()
+
     return args
 
 
@@ -19,35 +20,42 @@ def connect(username, password, host):
     conn.find_prompt()
     return conn
 
+
 def check_acl(conn):
-    #Enter send command here
-    conn.send_command()
+    # Enter send command here
+    conn.send_command("show ntp ass")
     time.sleep(2)
 
-    #Enter config commands in here - list
-    config_commands = []
+    # Enter config commands in here(list)
+    config_commands = [" "]
     output = conn.send_config_set(config_commands)
 
     return output
+
 
 def main():
     args = get_args()
 
     config = ConfigParser.ConfigParser()
     config.readfp(open(args.config))
-    dev = args.router
+    dev = args.routers
+    router = []
 
-    try:
-        username = config.get("router-man", "username")
-        password = config.get("router-man", "password")
-    except ConfigParser.NoOptionError:
-        username = args.username
-        password = config.get("router-man", "password")
+    for line in dev:
+        router.append(line.rstrip())
 
-    conn = connect(username=username, password=password, host=dev)
-    output = check_acl(conn)
+    for host in router:
+        try:
+            username = config.get("router-man", "username")
+            password = config.get("router-man", "password")
+        except ConfigParser.NoOptionError:
+            username = args.username
+            password = config.get("router-man", "password")
 
-    print output
+        conn = connect(username=username, password=password, host=host)
+        output = check_acl(conn)
+
+        print output
 
 
 if __name__ == '__main__':
