37fc26dc1c408c6515c0fecd72a4bbb93881338c
Trilogie <Trilogie00@gmail.com>
2019-09-10 20:52:16 -0700

    Random stuff

---
+++ hello_world.py
@@ -0,0 +1,29 @@
+first_name = "ada"
+last_name = "lovelace"
+full_name = f"{first_name} {last_name}"
+message = f"Hello, {full_name.title()}!"
+print(message)
+
+
+print("Python")
+print("\tPython")
+print("Languages:\n\tPython\n\tC\n\tJavascript")
+
+favorite_language = ' python '
+favorite_language = favorite_language.strip()
+print(favorite_language)
+
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+num = 4/2
+print(num)
+
+universe_age = 14_000_000
+print(universe_age)
+
+x, y, z = 0, 0, 0
+print(x, y, z)
+
+# Say Hello to everyone.
+print("Hello Python People!")
