d6d6ef68714cd5d8b6a6873cb56f32883354430e
Steve Pagan <spagan@stripe.com>
2019-09-11 20:42:04 -0700

    More random stuff

--- lists_practice.py
+++ lists_practice.py
@@ -22,8 +22,20 @@ print(motorcycles)
 motorcycles.insert(0, 'ducati')
 print(motorcycles)
 
-del motorcycles[0]
+last_owned = motorcycles.pop()
+print(f"The last motorcycle I owned was a {last_owned.title()}.")
+
+first_owned = motorcycles.pop(0)
+print(f"The first motorcycle I owned was a {first_owned.title()}.")
+
+motorcycles.remove('honda')
 print(motorcycles)
 
-del motorcycles[1]
+too_expensive = 'yamaha'
+motorcycles.remove(too_expensive)
 print(motorcycles)
+print(f"\nA {too_expensive.title()} is too expensive for me.")
+
+
+
+
