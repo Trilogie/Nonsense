8f3dfe5cbe69a2267ae6883b973a289e266a1f75
Trilogie <Trilogie00@gmail.com>
2019-09-11 19:52:28 -0700

    Random stuff

--- lists_practice.py
+++ lists_practice.py
@@ -4,3 +4,26 @@ print(bicycles[-1].title())
 message = f"My first bicycle was a {bicycles[0].title()}."
 print(message)
 
+motorcycles = ['honda', 'yamaha', 'suzuki']
+print(motorcycles)
+
+motorcycles[0] = 'ducati'
+print(motorcycles)
+
+motorcycles.append('honda')
+print(motorcycles)
+
+motorcycles = []
+motorcycles.append('honda')
+motorcycles.append('yamaha')
+motorcycles.append('suzuki')
+print(motorcycles)
+
+motorcycles.insert(0, 'ducati')
+print(motorcycles)
+
+del motorcycles[0]
+print(motorcycles)
+
+del motorcycles[1]
+print(motorcycles)
