a56fa2762f1e18214fa47f16ca1bb270b29b972c
Trilogie <Trilogie00@gmail.com>
2019-09-16 20:54:51 -0700

    Random stuff

---
+++ toppings.py
@@ -0,0 +1,12 @@
+requested_topping = [ 'mushrooms', 'onions', 'pineapple']
+
+if requested_topping != 'anchovies':
+    print("Hold the anchovies!")
+
+answer = 17
+if answer != 42:
+    print("That is not the correct answer. Please try again!")
+
+car = 'subaru'
+print("Is car == 'subaru'? I predict True.")
+print(car == 'subaru')
