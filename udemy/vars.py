b600d0d0f7a13539226b27954ed7bdf24d7bf9f9
Trilogie <Trilogie00@gmail.com>
2020-03-18 23:57:50 -0700

    Move files to new directory

---
+++ udemy/vars.py
@@ -0,0 +1,18 @@
+a = "    Cisco Switch    "
+
+print(a.index("i"))
+print(a.count("i"))
+print(a.find("sco"))
+print(a.find("xyz"))
+print(a.lower())
+print(a.upper())
+print(a.startswith("C"))
+print(a.endswith("h"))
+print(a.endswith("q"))
+print(a.strip())
+print(a.replace(" ", ""))
+
+d = "Cisco,Juniper,HP,Avaya,Nortel"
+string = d.split(",")
+print(string)
+
