20c14a712d647f70995850f3fc3b9e7a2038b42e
Trilogie <Trilogie00@gmail.com>
2020-03-19 18:54:39 -0700

    Move files to new directory

--- udemy/vars.py
+++ udemy/vars.py
@@ -16,3 +16,21 @@ d = "Cisco,Juniper,HP,Avaya,Nortel"
 string = d.split(",")
 print(string)
 
+x = "Cisco"
+y = " switch"
+print(x+y)
+
+print("o" in x)
+
+k = "Cisco model: {2}, {1} WAN Slots, IOS {0}".format("2600XM", 2, 12.4)
+print(k)
+
+model = "2600XM"
+slots = 4
+ios = 12.3
+
+print(f"Cisco Model: {model.lower()}, {slots * 2} WAN Slots, IOS {ios}")
+
+string1 = "O E2 10.110.8.9 [160/5]"
+print(string1[::-1])
+
